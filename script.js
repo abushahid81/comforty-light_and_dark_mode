778ae595b33f7afc1971c3e9124519d28932cd56
abushahid81 <abushahidsardar81@gmail.com>
2024-02-10 04:42:19 +0530

    javascript

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-let togglebtn = document.getElementById("toggle-btn");
-let body = document.body;
+// let togglebtn = document.getElementById("toggle-btn");
+// let body = document.body;
 
-togglebtn.addEventListener("click", function(){
-    body.classList.toggle("dark-mode");
-})
+// togglebtn.addEventListener("click", function(){
+//     body.classList.toggle("dark-mode");
+// })
+
+$(".form-check-input").click(function(){
+    $("button,h1, h2, h4, h5, p, a").toggleClass("change-text");
+    $("body").toggleClass("toggle-btn");
+    });
